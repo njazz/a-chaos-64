45cb20e14eeffd297d305af011d777680060a13a
Alex Nadzharov <anadjarov@gmail.com>
2019-03-15 18:44:26 +0300

    fix 2

--- src/extra/Files.h
+++ src/extra/Files.h
@@ -8,9 +8,4 @@
 #define __FILES__
 typedef void FSRef;
 
-// workaround for old types in objects' source code
-#ifdef WIN_VERSION
-typedef unsigned char Boolean;
-#endif
-
 #endif /* compatibility_h */
