541788b94780d7ef2823fedefbe0aa575f2bf73f
Alex <anadjarov@gmail.com>
2025-08-14 11:59:00 +0200

    scripts updated

--- scripts/install-package-8-osx.sh
+++ scripts/install-package-8-osx.sh
@@ -6,4 +6,4 @@ cd ..
 mkdir ~/Documents/Max\ 8/Packages/a-chaos-64
 mkdir ~/Documents/Max\ 8/Packages/a-chaos-64/externals
 
-cp -r package/a-chaos-64 ~/Documents/Max\ 7/Packages/
+cp -r package/a-chaos-64 ~/Documents/Max\ 8/Packages/
